5d97be98282a22370ad2f7f3a8ec6f0c7873fd6f
miroslavs <miroslavs@srv-bsf-trm03.gw-4u.com>
2024-04-04 10:58:32 +0300

    Fix for the masking in case of non Latin letters.

--- nuvei_checkout.php
+++ nuvei_checkout.php
@@ -2716,9 +2716,9 @@ class Nuvei_Checkout extends PaymentModule
             if (in_array($key, $fields['ips'])) {
                 $value = rtrim(long2ip(ip2long($value) & (~255)), "0")."x";
             } elseif (in_array($key, $fields['names'])) {
-                $value = substr($value, 0, 1) . '****';
+                $value = mb_substr($value, 0, 1) . '****';
             } elseif (in_array($key, $fields['emails'])) {
-                $value = '****' . substr($value, 4);
+                $value = '****' . mb_substr($value, 4);
             } elseif (in_array($key, $fields['address'])
                 || in_array($key, $fields['others'])
             ) {
