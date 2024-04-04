0db03747d0abdf313de9926a18b2a33e1f5c52c5
miroslavs <miroslavs@srv-bsf-trm03.gw-4u.com>
2024-04-04 12:51:44 +0300

    Fix for the masking in case of empty data is passed.

--- nuvei_checkout.php
+++ nuvei_checkout.php
@@ -1397,7 +1397,7 @@ class Nuvei_Checkout extends PaymentModule
             }
         }
         elseif(is_object($data)) {
-            if ($mask_details) {
+            if ($mask_details && !empty($data)) {
                 // clean possible objects inside array
                 $data = json_decode(json_encode($data), true);
 
